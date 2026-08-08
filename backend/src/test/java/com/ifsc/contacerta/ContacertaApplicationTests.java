e4a8de24945fe9d729dd611c37c0abdb139fcc19
Luiz Henrique <lhmendes0911@gmail.com>
2026-08-07 21:12:25 -0300

    iniciando backend

---
+++ backend/src/test/java/com/ifsc/contacerta/ContacertaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifsc.contacerta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContacertaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
